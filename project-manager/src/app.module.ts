540962f1f78521d8f642a673f1b40db3a565222f
ChisaeHwang <cwh73090@naver.com>
2022-08-09 22:50:35 +0900

    basic setting

--- project-manager/src/app.module.ts
+++ project-manager/src/app.module.ts
@@ -1,10 +1,26 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config'; // 환경 변수 사용 가능
+import { MongooseModule } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // 다른 모듈에서 config 모듈 가져올 필요 x
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure() {
+    const DEBUG = process.env.MODE === 'dev' ? true : false;
+    mongoose.set('debug', DEBUG); // mongoose query가 찍힘
+  }
+}
