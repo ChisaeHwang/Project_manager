78fb9a0e57e65bca097136c98d065f1d9b2a3b08
ChisaeHwang <cwh73090@naver.com>
2022-08-12 21:38:26 +0900

    new setting

---
+++ project/public/js/script.js
@@ -0,0 +1,57 @@
+const socket = io('/cahtting');
+
+const getElementById = (id) => document.getElementById(id) || null;
+
+//* get DOM element
+const helloStrangerElement = getElementById('hello_stranger');
+const chattingBoxElement = getElementById('chatting_box');
+const formElement = getElementById('chat_form');
+
+//* global socket handler
+socket.on('user_connected', (username) => {
+  drawNewChat(`${username} connected!`);
+});
+socket.on('new_chat', (data) => {
+  const { chat, username } = data;
+  drawNewChat(`${username}: ${chat}`);
+});
+
+//* event callback function
+const handleSumbit = (event) => {
+  event.preventDefault();
+  const inputValue = event.target.elements[0].value;
+  console.log(inputValue);
+  if (inputValue !== '') {
+    socket.emit('submit_chat', inputValue);
+    // 화면에 그리기
+    drawNewChat(`me : ${inputValue}`);
+    event.target.elements[0].value = '';
+  }
+};
+
+//* draw functions
+const drawHelloStranger = (username) =>
+  (helloStrangerElement.innerText = `Hello ${username} Stranger :)`);
+const drawNewChat = (message) => {
+  const wrapperChatBox = document.createElement('div');
+  const chatBox = `
+      <div>
+        ${message}
+      </div>
+      `;
+  wrapperChatBox.innerHTML = chatBox;
+  chattingBoxElement.append(wrapperChatBox);
+};
+function helloUser() {
+  const username = prompt('What is your name?');
+  socket.emit('new_user', username, (data) => {
+    drawHelloStranger(data);
+  });
+}
+
+function init() {
+  helloUser();
+  formElement.addEventListener('submit', handleSumbit);
+}
+
+init();
