6099e720767426de6c8827483b19695721b3d1f2
ChisaeHwang <cwh73090@naver.com>
2022-08-30 19:13:29 +0900

    Controller Pattern add

--- project/src/app.controller.ts
+++ project/src/app.controller.ts
@@ -6,6 +6,11 @@ export class AppController {
   constructor(private appService: AppService) {}
 
   @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get()
   @Render('index')
   login() {
     return;
