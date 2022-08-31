e99a9cc88074d0c2217d4c152be3f1d852f4b794
ChisaeHwang <cwh73090@naver.com>
2022-08-31 21:02:56 +0900

    add signup funtion

--- project/src/app.controller.ts
+++ project/src/app.controller.ts
@@ -1,4 +1,6 @@
-import { Get, Controller, Render } from '@nestjs/common';
+import { Req } from '@nestjs/common';
+import { Get, Controller, Render, Param, Body } from '@nestjs/common';
+import { Request } from 'express';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +8,14 @@ export class AppController {
   constructor(private appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHello(
+    @Req() req: Request,
+    @Body() body,
+    @Param() param: { id: string; name: string },
+  ): string {
+    console.log(param);
+    console.log(body);
+    console.log(req);
     return this.appService.getHello();
   }
 
--- project/src/main.ts
+++ project/src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -11,6 +12,7 @@ async function bootstrap() {
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new HttpExceptionFilter());
   const PORT = process.env.PORT;
   await app.listen(PORT);
 }
